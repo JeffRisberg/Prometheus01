78f8897385e73bec254cab950e7eab077528fd69
JeffRisberg <jrisberg@aol.com>
2020-09-16 19:00:51 -0700

    clarified the metrics

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -18,63 +18,64 @@ import java.io.IOException;
 public class Main {
 
   static class HelloServlet extends HttpServlet {
-    private static final Counter helloRequests = Counter.build()
-      .name("hello_worlds_total")
-      .help("Hello Worlds Requested.")
-      .register();
+    private static final Counter helloRequests =
+        Counter.build().name("hello_worlds_total").help("Hello Worlds Requested.").register();
 
-    private static final Histogram helloRequestLatency = Histogram.build()
-      .name("hello_requests_latency_seconds")
-      .help("Hello Request latency in seconds.")
-      .register();
+    private static final Histogram helloRequestLatency =
+        Histogram.build()
+            .name("hello_requests_latency_seconds")
+            .help("Hello Request latency in seconds.")
+            .register();
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-      throws ServletException, IOException {
+        throws ServletException, IOException {
 
-      helloRequestLatency.time(() -> {
-        // Increment the number of requests.
-        helloRequests.inc();
+      helloRequestLatency.time(
+          () -> {
+            // Increment the number of requests.
+            helloRequests.inc();
 
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getMethod());
-        System.out.println("count " + helloRequests.get());
+            System.out.println(req.getRequestURI());
+            System.out.println(req.getMethod());
+            System.out.println("count " + helloRequests.get());
 
-        try {
-          resp.getWriter().println("Hello World!");
-        } catch (Exception e) {
-        }
-      });
+            try {
+              resp.getWriter().println("Hello World!");
+            } catch (Exception e) {
+            }
+          });
     }
   }
 
   static class GoodbyeServlet extends HttpServlet {
-    private static final Counter goodbyeRequests = Counter.build()
-      .name("goodbye_worlds_total")
-      .help("Goodbye Worlds Requested.").register();
+    private static final Counter goodbyeRequests =
+        Counter.build().name("goodbye_worlds_total").help("Goodbye Worlds Requested.").register();
 
-    private static final Histogram goodbyeRequestLatency = Histogram.build()
-      .name("goodbye_requests_latency_seconds")
-      .help("Goodbye Request latency in seconds.")
-      .register();
+    private static final Histogram goodbyeRequestLatency =
+        Histogram.build()
+            .name("goodbye_requests_latency_seconds")
+            .help("Goodbye Request latency in seconds.")
+            .register();
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
-      throws ServletException, IOException {
+        throws ServletException, IOException {
 
-      goodbyeRequestLatency.time(() -> {
-        // Increment the number of requests.
-        goodbyeRequests.inc();
+      goodbyeRequestLatency.time(
+          () -> {
+            // Increment the number of requests.
+            goodbyeRequests.inc();
 
-        System.out.println(req.getRequestURI());
-        System.out.println(req.getMethod());
-        System.out.println("count " + goodbyeRequests.get());
+            System.out.println(req.getRequestURI());
+            System.out.println(req.getMethod());
+            System.out.println("count " + goodbyeRequests.get());
 
-        try {
-          resp.getWriter().println("Goodbye World!");
-        } catch (Exception e) {
-        }
-      });
+            try {
+              resp.getWriter().println("Goodbye World!");
+            } catch (Exception e) {
+            }
+          });
     }
   }
 
